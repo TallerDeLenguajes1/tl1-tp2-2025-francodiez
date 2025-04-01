51d0aa3896086e99df54b0cd3bf1f72eca924f44
Franco <francodiezgomez@gmail.com>
2025-04-01 13:44:01 -0300

    Ejercicio 3

---
+++ tp2_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 5
+#define M 7
+
+
+int main(){
+    srand(time(NULL));
+    int i,j;
+    int mt[N][M];
+    
+    //con *(mt+1)+j me da la direccion mt[i][j]. Con *(*(mt+i)+j) me da el valor de mt[i][j]
+    for(i = 0;i<N; i++){
+      for(j = 0;j<M; j++){
+      *(*(mt+i)+j)=1+rand()%100;
+      printf("%d ", *(*(mt+i)+j));
+      }
+    printf("\n");
+    }
+
+    getchar();
+    return 0;
+}
+
