d10a343d67298702b97023283dc25987870e65a5
Franco <francodiezgomez@gmail.com>
2025-04-05 22:43:29 -0300

    Implementacion de la primera funcion

--- tp2_4.c
+++ tp2_4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 struct compu {
     int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
@@ -10,9 +11,11 @@ struct compu {
 };
 typedef struct compu compu;
 
+void listarPCs(compu pcs[], int cantidad);
+
 int main(){
     srand(time(NULL));
-    int i;
+    int i,cantidad=5;
     compu computadora[4];
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
     
@@ -20,9 +23,26 @@ int main(){
         computadora[i].velocidad=1+rand()%3;
         computadora[i].anio=2015+rand()%(2024-2015+1);
         computadora[i].cantidad_nucleos=1+rand()%8;
-        computadora[i].tipo_cpu=&tipos[rand()%6];
+        computadora[i].tipo_cpu=&tipos[rand()%6][10];
     }
+
+    listarPCs(computadora, cantidad);
 
     getchar();
     return 0;
 }
+
+void listarPCs(compu pcs[], int cantidad){
+    int i;
+    cantidad=5;
+    for (i=0;i<cantidad;i++){
+        printf("Computadora %d:",i+1);
+        printf("\nVelocidad de procesamiento: %d GHz",pcs[i].velocidad);
+        printf("\nAnio de fabricacion: %d",pcs[i].anio);
+        printf("\nCantidad de nucleos: %d",pcs[i].cantidad_nucleos);
+        printf("\nTipo de procesador:");
+        puts(pcs[i].tipo_cpu);
+        printf("\n");
+    }    
+}
+
